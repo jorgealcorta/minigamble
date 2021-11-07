01908156c2af2e71cb95ff8ce998166c55b8964d
jorgealcorta <jorgealcorta@opendeusto.es>
2021-11-07 19:50:16 +0100

    Nueva estructura de clases
    Cambio de extends JFrame a extends Canvas

---
+++ minigamble/src/minigamble/VentanaPrincipal.java
@@ -0,0 +1,19 @@
+package minigamble;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+
+public class VentanaPrincipal extends Canvas{
+	
+	public VentanaPrincipal(String title, Game game) {
+		
+	}
+
+};
