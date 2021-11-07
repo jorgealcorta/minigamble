12eb1964a0440480e707117ee109e89926f41e3a
nicllop <nicllop@opendeusto.es>
2021-11-07 13:13:29 +0100

    start

---
+++ minigamble/src/minigamble/ventanaPrincipal.java
@@ -0,0 +1,31 @@
+package minigamble;
+
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+
+public class ventanaPrincipal extends JFrame {
+	
+	
+	private JButton bStart;
+	private JPanel pPrincipal;
+	
+	
+	
+	
+	
+	
+	public ventanaPrincipal() {
+		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setTitle("mingamble.start");
+		
+		pPrincipal = new JPanel();
+		
+		
+	}
+	
+};
