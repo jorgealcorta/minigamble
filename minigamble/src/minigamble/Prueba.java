408332207e33d739c7da5995f2acdb349773c169
nicllop <nicllop@opendeusto.es>
2021-11-07 11:54:16 +0100

    wewe

--- minigamble/src/minigamble/Prueba.java
+++ minigamble/src/minigamble/Prueba.java
@@ -3,6 +3,6 @@ package minigamble;
 public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba");
-		System.out.println("comprobacion imports unai");
+		System.out.println("comprobacion imports unaiii");
 	}
 }
