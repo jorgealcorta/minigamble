944c7f00bd32addb47b2aa407d69e3347b809842
Unai Igartua <unaiigartua@opendeusto.es>
2021-11-07 20:02:47 +0100

    Corrección

--- minigamble/src/minigamble/VentanaPrincipal.java
+++ minigamble/src/minigamble/VentanaPrincipal.java
@@ -10,54 +10,10 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
-public class VentanaPrincipal extends JFrame {
+public class VentanaPrincipal extends Canvas{
 	
-	
-	private JButton bStart;
-	private JPanel pPrincipal;
-	private JFrame frame;
-	private ImageIcon background;
-	
-	
-	
-	
-	
-	
-	public VentanaPrincipal() {
-		
-		
-		frame = new JFrame("minigamble.start");
-		frame.setSize(1200, 700);
-		
-		
-		frame.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		
-		
-		try { 
-			
-			background = new ImageIcon( VentanaPrincipal.class.getResource("multimedia/background.png").toURI().toURL() ); 
-			bStart = new JButton( new ImageIcon( VentanaPrincipal.class.getResource("multimedia/yellow_button1.png").toURI().toURL() ) );
-		} catch (Exception e1) {  // Si hay error, botones texto
-			bStart = new JButton( "Start" );
-			JLabel background = new JLabel();
-		}
-		
-		
-		Image auxImg = background.getImage();
-		Image fBackground = auxImg.getScaledInstance(frame.getWidth(), frame.getHeight(), Image.SCALE_SMOOTH);
-		ImageIcon bIcon = new ImageIcon(fBackground);
-		JLabel bLabel = new JLabel(bIcon);
-		
-		//ImageIcon final  = new ImageIcon(dBackground);
-		
-		frame.add(bStart, BorderLayout.CENTER);
-		frame.add(bLabel);
-		frame.setVisible(true);
-		pPrincipal = new JPanel();
-		 
+	public VentanaPrincipal(String title, Game game) {
 		
 	}
-	
+
 };
---
+++ minigamble/src/minigamble/minigamble.java
@@ -0,0 +1,13 @@
+package minigamble;
+
+public class minigamble {
+
+	public static void main(String[] args) {
+		
+		
+		VentanaPrincipal ventana = new VentanaPrincipal(null, null);
+		
+		
+	}
+
+}
