a0431c2c621b5089407f9bc92574ec55f67e4981
jorgealcorta <jorgealcorta@opendeusto.es>
2021-11-07 23:16:50 +0100

    Game loop, métodos tick y render creados

--- minigamble/src/minigamble/Game.java
+++ minigamble/src/minigamble/Game.java
@@ -1,6 +1,7 @@
 package minigamble;
 
 import java.awt.Canvas;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable{
 	
@@ -33,6 +34,45 @@ public class Game extends Canvas implements Runnable{
 	@Override
 	public void run() {
 		
+		//GAME LOOP//
+		
+		long lastTime = System.nanoTime(); // In miliseconds.
+		double amountOfTicks = 60.0; // Ticks per second.
+		double ns = 1000000000 / amountOfTicks; // Nanoseconds per tick.
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		
+		while(running) {
+			long now = System.nanoTime(); // In miliseconds.
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) { // delta = 1 significa que ha pasado un tick.
+				 tick();
+				 delta--; // Reseteamos delta a 0.
+			}
+			if(running)
+				render(); // Refresca la ventana, ejecutando render.
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) { // Escribe los FPS una vez cada segundo.
+				timer += 1000; // Si la diferencia entre el tiempo real y el timer es de más de 1000ms, implica que ha pasado un segundo.
+				System.out.println("FPS : " + frames); // Printea los frames que se han registrado en ese segundo.
+				frames = 0; // Resetea frames.
+			}
+		}
+		stop();
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy(); // El Buffer Strategy representa el mecanismo para organizar memoria compleja en una ventana.
+		if(bs == null) { // El valor default es null.
+			this.createBufferStrategy(3); // The number of buffers it creates.
+		}
 	}
 
 	public static void main(String[] args) {
