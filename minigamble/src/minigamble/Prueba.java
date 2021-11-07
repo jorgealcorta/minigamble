5256ce77db0c3e2148a4859434387c433491b0ae
nicllop <nicllop@opendeusto.es>
2021-11-07 11:24:22 +0100

    comprobacion
    comprobacion

--- minigamble/src/minigamble/Prueba.java
+++ minigamble/src/minigamble/Prueba.java
@@ -3,6 +3,6 @@ package minigamble;
 public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba");
-		System.out.println("prueba commit 1.1");
+		System.out.println("esto es comprobacion");
 	}
 }
