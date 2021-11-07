9bbfdd63f9ec4060c11d13295b079693ab95cc83
jorgealcorta <jorgealcorta@opendeusto.es>
2021-11-07 11:59:58 +0100

    prueba jorge push 1

--- minigamble/src/minigamble/Prueba.java
+++ minigamble/src/minigamble/Prueba.java
@@ -4,5 +4,6 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba");
 		System.out.println("comprobacion imports unaiii");
+		System.out.println("prueba push jorge");
 	}
 }
