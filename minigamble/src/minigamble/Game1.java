dc8e1c5711eaa4e75a68bbb3dc2e951ca38a7013
jorgealcorta <jorgealcorta@opendeusto.es>
2021-12-05 19:41:15 +0100

    Creación de clases Partida y Minijuego

--- minigamble/src/minigamble/Game1.java
+++ minigamble/src/minigamble/Game1.java
@@ -11,4 +11,5 @@ public class Game1 { // SIMON SAYS
 	}
 	
 	
+		
 }
--- minigamble/src/minigamble/Partida.java
+++ minigamble/src/minigamble/Partida.java
@@ -1,6 +1,41 @@
 package minigamble;
 
+import java.util.ArrayList;
+import java.lang.Math;
+
+
+import minigamble.Game.ESTADO;
+
 public class Partida {
+	
+	public static int puntuacionGeneral = 0;
+	public static int vidas = 3;
+	
+	public static ArrayList<Minijuego> minisJugados = new ArrayList<Minijuego>();
+		
+	
+	public Partida(int puntuacion, int vidasRestadas, String idUsuario, Minijuego miniJugado) { //meter arraylist como atributo
+		
+		minisJugados.add(miniJugado);
+		puntuacionGeneral += puntuacion;
+		vidas -= vidasRestadas; //OJO, ESTA RESTANDO
+		
+		ArrayList<ESTADO> minijuegos = new ArrayList<ESTADO>();
+
+		minijuegos.add(ESTADO.Game1);
+		minijuegos.add(ESTADO.Game2);
+		minijuegos.add(ESTADO.Game3);
+		minijuegos.add(ESTADO.Game4);
+		minijuegos.add(ESTADO.Game5);
+		minijuegos.add(ESTADO.Game6);
+		
+		int indexAleatorio = (int) (Math.random() * 5);
+		
+		Game.estadoJuego = minijuegos.get(indexAleatorio);
+		
+		
+	}
+		
 	
 	/*
 	
@@ -25,7 +60,5 @@ public class Partida {
 	
 	*/
 	
-		
-	}
 	
 }
