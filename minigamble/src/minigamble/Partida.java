5e0193d2a5309aaf2b3655f8b2169921924efdda
nicllop <nicllop@opendeusto.es>
2021-12-05 19:14:40 +0100

    Update Partida.java

---
+++ minigamble/src/minigamble/Partida.java
@@ -0,0 +1,31 @@
+package minigamble;
+
+public class Partida {
+	
+	/*
+	
+	public Partida(puntuacion, vidas, arraylst[juego], idUsuariho) {
+	
+		randon = new rand = juego x
+		
+		juego = juegox.run()
+		
+		puntuntuacion = puntucion+juego
+		vidss=nivad-juego
+		arraylist+juego
+		
+		if tuienevidas
+			partida(nuevpun newvidas newarraylist)
+			
+		else
+			
+			
+				
+	}
+	
+	*/
+	
+		
+	}
+	
+}
