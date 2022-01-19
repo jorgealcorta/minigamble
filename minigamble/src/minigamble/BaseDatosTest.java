e1ac3ef05830b66cdbd01aa5e9bb023952406c79
Unai Igartua <unaiigartua@opendeusto.es>
2022-01-19 20:58:42 +0100

    testInsertarPartidas

--- minigamble/src/minigamble/BaseDatosTest.java
+++ minigamble/src/minigamble/BaseDatosTest.java
@@ -1,6 +1,8 @@
 package minigamble;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.junit.After;
@@ -23,13 +25,22 @@ public class BaseDatosTest {
 	public void testGetPartidas() {
 		HashMap<Integer, String> lP = BaseDatos.getPartidas();
 		assertEquals( 9, lP.size() );
-		int antId = -1;
+		int antId = 0;
 		for (Integer id : lP.keySet()) {
 			assertTrue( id > antId );
 			antId = id;
 		}
 	}
 	
-	
+	@Test
+	public void testInsertarPartida() {
+		HashMap<Integer, String> lP = BaseDatos.getPartidas();
+		int antId = 0;
+		for (Integer id : lP.keySet()) {
+			assertTrue( id > antId );
+			antId = id;
+		}
+		assertTrue( BaseDatos.insertarPartida("Juan") >= antId);
+	}
 
 }
