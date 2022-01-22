492202ac5d7fcdf820af941ae4c7f0323f35827c
jorgealcorta <jorgealcorta@opendeusto.es>
2022-01-23 00:54:07 +0100

    Javadoc Laberinto

--- minigamble/src/minigamble/Laberinto.java
+++ minigamble/src/minigamble/Laberinto.java
@@ -7,24 +7,40 @@ public class Laberinto {
 	private Image image;
 	private int id;
 	
+	/**
+	 * @param image imagen del laberinto
+	 * @param id identificador del laberinto
+	 */
 	public Laberinto(Image image, int id) {
 
 		this.image = image;
 		this.id = id;
 	}
 
+	/**
+	 * @return devuelve la imagen del laberinto
+	 */
 	public Image getImage() {
 		return image;
 	}
 
+	/**
+	 * @param image cambia la imagen del laberinto
+	 */
 	public void setImage(Image image) {
 		this.image = image;
 	}
 
+	/**
+	 * @return devuelve el identificador del laberinto
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * @param id devuelve el identificador del laberinto
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
