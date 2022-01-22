06db6adfec726cb75b3fb58f50d0fd4a680494ac
jorgealcorta <jorgealcorta@opendeusto.es>
2022-01-23 00:23:08 +0100

    javadoc Diana

--- minigamble/src/minigamble/Diana.java
+++ minigamble/src/minigamble/Diana.java
@@ -21,34 +21,67 @@ public class Diana {
 		this.rota = rota;
 	}
 
+	/**
+	 * @return devuelve la coordenada X de una diana
+	 */
 	public int getX() {
 		return x;
 	}
 	
+	/**
+	 * @param x cambia la coordenada X de una diana
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
+	
+	/**
+	 * @return devuelve la coordenada Y de una diana
+	 */
 	public int getY() {
 		return y;
 	}
+	
+	/**
+	 * @param y cambia la coordenada y de una diana
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
+	
+	/**
+	 * @return devuelve el tamanyo de una diana
+	 */
 	public double getSize() {
 		return size;
 	}
+	
+	/**
+	 * @param size cambia el tamanyo de una diana
+	 */
 	public void setSize(int size) {
 		this.size = size;
 	}
 	
+	
+	/**
+	 * @return devuelve si una diana esta rota
+	 */
 	public boolean isRota() {
 		return rota;
 	}
 
+	
+	/**
+	 * @param rota cambia si una diana esta rota o no
+	 */
 	public void setRota(boolean rota) {
 		this.rota = rota;
 	}
 
+	/**
+	 * Metodo toString para visualizar datos de una diana
+	 */
 	@Override
 	public String toString() {
 		return "Diana [x=" + x + ", y=" + y + ", size=" + size + ", rota=" + rota + "]";
